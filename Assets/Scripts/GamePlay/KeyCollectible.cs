1704d698838e60b0601604fc46f296a8de5f8d7d
Parva <parvadesai404@gmail.com>
2023-09-30 00:13:36 +0530

    fix: Updated key collider to isTriggers to fix the collision force getting applied to player during key collection

--- Assets/Scripts/GamePlay/KeyCollectible.cs
+++ Assets/Scripts/GamePlay/KeyCollectible.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class KeyCollectible : MonoBehaviour
 {
-   private void OnCollisionEnter2D(Collision2D obj)
-   {
-       PlayerController playerController = obj.gameObject.GetComponent<PlayerController>();
-       
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        PlayerController playerController = obj.gameObject.GetComponent<PlayerController>();
+        
         if(playerController != null)
         {
             playerController.PickUpKey();
             Destroy(gameObject); 
         }
-   }
+    }
 }
