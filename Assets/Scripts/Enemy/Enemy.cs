115b1b1dcb97f3793848ad206ec3b04e8efd4595
Parva <parvadesai404@gmail.com>
2023-11-04 23:22:00 +0530

    fix: Issues with enemy flipping direction

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -33,6 +33,8 @@ public class Enemy : MonoBehaviour
     {
         isDying = false;
         damageable.SetHealth(damageable.startingHealth);
+        
+        isMovingRight = transform.position.x < patrolSpots[currPatrolSpotIndex].position.x;
     }
 
     protected void FixedUpdate()
